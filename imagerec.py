fce1286235cd8208554fa773bfac696815ff27ac
Devarun <48183986+Devarungoswami@users.noreply.github.com>
2019-04-09 01:49:45 +0530

    Update imagerec.py

--- imagerec.py
+++ imagerec.py
@@ -1,12 +1,15 @@
+#HANDWRITING RECOGNITION
 import tensorflow as tf
+#mnist is a dataset of handwritten digits
 mnist =tf.keras.datasets.mnist
 
 
 import tensorflow as tf
 mnist =tf.keras.datasets.mnist
 
-
+#loading the dataset
 (x_train,y_train),(x_test,y_test)=mnist.load_data()
+#normalizing
 x_train= tf.keras.utils.normalize(x_train,axis=1)
 x_test= tf.keras.utils.normalize(x_test,axis=1)
 
@@ -18,6 +21,7 @@ plt.show()
 
 
 import keras
+#adding layers 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
@@ -27,6 +31,7 @@ model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 model.compile(optimizer='adam', 
 loss='sparse_categorical_crossentropy',
 metrics=['accuracy'])
+#fit
 model.fit(x_train,y_train,epochs=3)
 
 import numpy as np
@@ -35,6 +40,7 @@ predictions=model.predict([x_test])
 print(predictions)
 print(np.argmax(predictions[0]))
 
+#display final image
 plt.imshow(x_test[0])
 plt.show()
 
